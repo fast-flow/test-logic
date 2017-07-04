7efd206cf2209c33b6d8e8e77357f8bcebb7a4bc
Nimo <nimo.jser@gmail.com>
2017-07-04 17:14:15 +0800

    regexp

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,14 +1,43 @@
-import { Component } from "react"
-import Testlogic from "../lib/index"
-import Test from 'react-addons-test-utils'
-import { delay } from "./util"
-it('className', function (done) {
-    var app = Test.renderIntoDocument(<Testlogic className="myclass" />)
-    delay(done,[
-        function checkClassName() {
-            expect(Test.scryRenderedDOMComponentsWithClass(app, 'myclass').length)
-                .toEqual(1)
-            arguments[0]()
+import TextLogic from "../lib/index"
+import expect from "expect.js"
+const test = new TextLogic()
+it('regexp be true', function (done) {
+    test.check({
+        value: 'abcd1',
+        label: '用户名',
+        test: [
+            {
+                regexp: /\d/,
+                be: false,
+                msg: '{{label}}不能包含数字'
+            }
+        ],
+        finish: function (fail, info) {
+            expect(fail).to.eql(true)
+            expect(info.source.length).to.eql(1)
+            expect(info.source[0].error).to.eql(true)
+            expect(info.source[0].msg).to.eql('用户名不能包含数字')
+            done()
         }
-    ])
+    })
+})
+it('regexp be true', function (done) {
+    test.check({
+        value: 'abcd',
+        label: '用户名',
+        test: [
+            {
+                regexp: /\d/,
+                be: true,
+                msg: '{{label}}必须包含数字'
+            }
+        ],
+        finish: function (fail, info) {
+            expect(fail).to.eql(true)
+            expect(info.source.length).to.eql(1)
+            expect(info.source[0].error).to.eql(true)
+            expect(info.source[0].msg).to.eql('用户名必须包含数字')
+            done()
+        }
+    })
 })
