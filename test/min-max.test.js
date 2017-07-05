0bb9ab9c45865962e06eefeefc41dd1c6ad0c1ae
Nimo <nimo.jser@gmail.com>
2017-07-05 15:57:51 +0800

    max test

--- test/min-max.test.js
+++ test/min-max.test.js
@@ -45,3 +45,45 @@ it('min', function (done) {
         }
     })
 })
+it('max', function (done) {
+    test.check({
+        value: '6',
+        label: '年龄',
+        test: [
+            {
+                rule: 'number'
+            },
+            {
+                max: 4,
+                msg: '{{label}}不能大于{{self.max}}岁,当前输入{{value}}'
+            }
+        ],
+        finish: function (fail, info) {
+            expect(fail).to.eql(true)
+            expect(info.source.length).to.eql(2)
+            expect(info.source[0].error).to.eql(false)
+            expect(info.source[1].msg).to.eql('年龄不能大于4岁,当前输入6')
+            done()
+        }
+    })
+    test.check({
+        value: '4',
+        label: '年龄',
+        test: [
+            {
+                rule: 'number'
+            },
+            {
+                max: 4,
+                msg: '{{label}}不能大于{{self.max}}岁,当前输入{{value}}'
+            }
+        ],
+        finish: function (fail, info) {
+            expect(fail).to.eql(false)
+            expect(info.source.length).to.eql(2)
+            expect(info.source[0].error).to.eql(false)
+            expect(info.source[1].error).to.eql(false)
+            done()
+        }
+    })
+})
