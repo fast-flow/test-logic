b3f8975efccc469bac74445ff496827fb61f79ce
Nimo <nimo.jser@gmail.com>
2017-07-06 10:42:37 +0800

    minLength maxLength

--- test/minLength-maxLength.test.js
+++ test/minLength-maxLength.test.js
@@ -18,6 +18,23 @@ it('minLength', function (done) {
         }
     })
     test.check({
+        value: '1234',
+        label: '用户名',
+        test: [
+            {
+                minLength: 4,
+                msg: '{{label}}至少{{self.minLength}}位'
+            }
+        ],
+        finish: function (fail, info) {
+            expect(fail).to.eql(false)
+            expect(info.source[0].error).to.eql(false)
+        }
+    })
+    setTimeout(done, 10)
+})
+it('maxLength', function (done) {
+    test.check({
         value: '12345',
         label: '用户名',
         test: [
@@ -30,6 +47,20 @@ it('minLength', function (done) {
             expect(fail).to.eql(true)
             expect(info.source[0].error).to.eql(true)
             expect(info.source[0].msg).to.eql('用户名最多4位')
+        }
+    })
+    test.check({
+        value: '1234',
+        label: '用户名',
+        test: [
+            {
+                maxLength: 4,
+                msg: '{{label}}最多{{self.maxLength}}位'
+            }
+        ],
+        finish: function (fail, info) {
+            expect(fail).to.eql(false)
+            // expect(info.source[0].error).to.eql(false)
         }
     })
     setTimeout(done, 10)
