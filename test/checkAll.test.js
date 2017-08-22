91e07ae028400e2085409d4134eb41c0d90a6095
Nimo <nimo.jser@gmail.com>
2017-08-22 15:38:01 +0800

    queue break

--- test/checkAll.test.js
+++ test/checkAll.test.js
@@ -1,10 +1,45 @@
 import TextLogic from "../lib/index"
 import expect from "expect.js"
 const test = new TextLogic()
-it('checkAll', function (done) {
+it('checkAll 2 fail', function (done) {
     test.checkAll([
         {
-            value: 'abcd1',
+            value: '1abcd',
+            label: '用户名',
+            test: [
+                {
+                    regexp: /\d/,
+                    be: false,
+                    msg: '{{label}}不能包含数字'
+                }
+            ]
+        },
+        {
+            value: '二3',
+            label: '用户名',
+            test: [
+                {
+                    minByte: 4,
+                    msg: '{{label}}不能少{{self.minByte}}个英文|{{self.minByteHalf}}个中文,当前字节{{valueByte}}'
+                }
+            ]
+        }
+
+    ], {
+        finish: function(fail, errors, data) {
+            expect(fail).to.eql(true)
+            expect(errors[0].msg).to.eql('用户名不能包含数字')
+            expect(errors[1].msg).to.eql('用户名不能少4个英文|2个中文,当前字节3')
+            expect(errors.length).to.eql(2)
+            expect(data.length).to.eql(2)
+            done()
+        }
+    })
+})
+it('checkAll 1 fail', function (done) {
+    test.checkAll([
+        {
+            value: '1abcd',
             label: '用户名',
             test: [
                 {
@@ -26,11 +61,11 @@ it('checkAll', function (done) {
         }
 
     ], {
-        finish: function(fail, errors, source) {
+        finish: function(fail, errors, data) {
             expect(fail).to.eql(true)
             expect(errors[0].msg).to.eql('用户名不能包含数字')
             expect(errors.length).to.eql(1)
-            expect(source.length).to.eql(2)
+            expect(data.length).to.eql(2)
             done()
         }
     })
@@ -66,7 +101,7 @@ it('checkAll default queue:false', function (done) {
         }
 
     ], {
-        finish: function(fail, errors, source) {
+        finish: function(fail, errors, data) {
             expect(fail).to.eql(true)
             expect(errors[1].msg - errors[0].msg).to.be.within(0, 100)
             done()
@@ -104,9 +139,45 @@ it('checkAll default queue:true', function (done) {
 
     ], {
         queue: true,
-        finish: function(fail, errors, source) {
+        finish: function(fail, errors, data) {
             expect(fail).to.eql(true)
             expect(errors[1].msg - errors[0].msg).to.be.within(500, 600)
+            done()
+        }
+    })
+})
+
+
+it('checkAll every: false', function (done) {
+    test.checkAll([
+        {
+            value: '1abcd',
+            label: '用户名',
+            test: [
+                {
+                    regexp: /\d/,
+                    be: false,
+                    msg: '{{label}}不能包含数字'
+                }
+            ]
+        },
+        {
+            value: '二34',
+            label: '用户名',
+            test: [
+                {
+                    minByte: 4,
+                    msg: '{{label}}不能少{{self.minByte}}个英文|{{self.minByteHalf}}个中文,当前字节{{valueByte}}'
+                }
+            ]
+        }
+    ], {
+        queue: true,
+        every: false,
+        finish: function(fail, errors, data) {
+            expect(fail).to.eql(true)
+            expect(errors[0].msg).to.eql('用户名不能包含数字')
+            expect(errors.length).to.eql(1)
             done()
         }
     })
